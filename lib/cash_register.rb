4d8b5378ddf4a25da59bd7c8881f01855e4d74b2
Grace Shih <vocalise@gmail.com>
2018-05-16 04:24:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,9 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     self.total += (price)*quantity
-    quantity.times @list_of_items << title
+    quantity.times do 
+      @list_of_items << title
+    end
   end
   
   def apply_discount
