24d208afd6cd8f101c95d6c9305993e1977a2e6c
Grace Shih <vocalise@gmail.com>
2018-05-16 04:10:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
-
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total += (price)*quantity
+  end
+  
+  def apply_discount
+   # binding.pry
+    if self.discount != 0
+      self.total = self.total - self.total * (self.discount/100.0)
+      puts "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      puts "There is no discount to apply."
+    end
+  end
+end
